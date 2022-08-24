88ad77de46bbe52a8dfc994751bdfe49c804736d
Hassanahmed006 <freelancerhassan3@gmail.com>
2022-08-24 15:01:42 -0700

    Zomato

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="absolute-center max-width" style={{marginBottom: "20px"}}>Made with 🔥 by Hassan Ahmed</div>
+  );
+};
+
+export default Footer;
